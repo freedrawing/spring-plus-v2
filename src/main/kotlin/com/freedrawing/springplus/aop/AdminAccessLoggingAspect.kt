d7638b70fcaa65c5b35af141afec1f0fea06dd4a
seonguk_kang <dnr2144@gmail.com>
2025-01-17 17:38:34 +0900

    :laybug: fix: `AdminAccessLogginAspect` expression

---
+++ src/main/kotlin/com/freedrawing/springplus/aop/AdminAccessLoggingAspect.kt
@@ -0,0 +1,30 @@
+package com.freedrawing.springplus.aop
+
+import com.freedrawing.springplus.config.util.LoggerUtil
+import jakarta.servlet.http.HttpServletRequest
+import org.aspectj.lang.JoinPoint
+import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Before
+import org.springframework.stereotype.Component
+import org.springframework.web.context.request.RequestContextHolder
+import org.springframework.web.context.request.ServletRequestAttributes
+import java.time.LocalDateTime
+
+@Component
+@Aspect
+class AdminAccessLoggingAspect {
+
+    //    @Before("execution(* com.freedrawing.springplus.domain.user.controller..*(..))")
+    @Before("execution(* com.freedrawing.springplus.domain.user.controller.UserController.changeRole(..))")
+    fun logBeforeChangeRole(joinPoint: JoinPoint) {
+        val request = (RequestContextHolder.getRequestAttributes() as ServletRequestAttributes).request
+        val userId = request.getAttribute("userId") ?: "Unknown"
+        val requestUrl = request.requestURI
+        val requestTime = LocalDateTime.now()
+
+        LoggerUtil.log.info(
+            "[Admin Access] User ID: {}, Time: {}, URL: {}, Method: {}",
+            userId, requestTime, requestUrl, joinPoint.signature.name
+        )
+    }
+}
